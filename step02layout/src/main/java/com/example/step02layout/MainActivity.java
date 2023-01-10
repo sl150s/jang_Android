c31bf43b4d17dcf823fce89143c3e04dc104e121
jangheejin <sl150s@naver.com>
2023-01-10 12:19:48 +0900

    step02layout 레이아웃 속성 정리(정렬...)

--- step02layout/src/main/java/com/example/step02layout/MainActivity.java
+++ step02layout/src/main/java/com/example/step02layout/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.linear01);
+        setContentView(R.layout.linear02);
     }
 }
